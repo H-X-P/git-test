95d9299e6420d72820a324deb617e8179810fe2d
Workaholic <56997137+H-X-P@users.noreply.github.com>
2022-06-04 21:06:04 +0800

    fix some bug

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String args[]) {
         try {
-		System.out.println("Hello Java");
+		System.out.println("Hello World");
 	} catch (Exception e) {
 		System.out.println("Error!!!");
 	}
