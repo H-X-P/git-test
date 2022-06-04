b732f288f682b4108c7013df3b8b2b244f306937
HXP <hxp16315943@foxmail.com>
2022-06-04 17:29:53 +0800

    second commit

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String args[]) {
         try {
-		System.out.println("Hello World");
+		System.out.println("Hello Java");
 	} catch (Exception e) {
 		System.out.println("Error");
 	}
