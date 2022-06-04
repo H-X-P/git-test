dec0b44f24a4b63052ff2bdd84657b4fb4779d66
HXP <hxp16315943@foxmail.com>
2022-06-04 17:07:19 +0800

    hot-fix first commit

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        try {
+		System.out.println("Hello World");
+	} catch (Exception e) {
+		System.out.println("Error");
+	}
     }
 }
